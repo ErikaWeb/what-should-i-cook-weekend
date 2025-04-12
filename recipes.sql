57e5a156e80eb60b5a14a351e9a780258f171c42
ErikaWeb <55951181+ErikaWeb@users.noreply.github.com>
2025-04-12 13:37:22 +0100

    Add files via upload

---
+++ recipes.sql
@@ -0,0 +1,28 @@
+-- Find vegetarian meals
+
+SELECT Title, Cleaned_Ingredients 
+FROM `erika-4-sql.recipes.recipes` 
+WHERE LOWER(Cleaned_Ingredients) NOT LIKE '%chicken%'
+  AND LOWER(Cleaned_Ingredients) NOT LIKE '%beef%'
+  AND LOWER(Cleaned_Ingredients) NOT LIKE '%pork%'
+  AND LOWER(Cleaned_Ingredients) NOT LIKE '%bacon%'
+  AND LOWER(Cleaned_Ingredients) NOT LIKE '%fish%'
+LIMIT 10;
+
+
+-- Find me a random recipe for today
+
+SELECT Title, Cleaned_Ingredients, Instructions
+FROM `erika-4-sql.recipes.recipes`
+ORDER BY RAND()
+LIMIT 1;
+
+
+-- What can I cook with…”
+
+SELECT Title, Cleaned_Ingredients, Instructions
+FROM `erika-4-sql.recipes.recipes`
+WHERE LOWER(Cleaned_Ingredients) LIKE '%onion%'
+  AND LOWER(Cleaned_Ingredients) LIKE '%potato%'
+  AND LOWER(Cleaned_Ingredients) LIKE '%egg%'
+LIMIT 10;
